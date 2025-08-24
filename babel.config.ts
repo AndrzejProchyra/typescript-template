1e277cdfcd404ea356adf47b2ed986d728096a68
Andrzej Prochyra <andrzej@prochyra.io>
2025-08-24 19:47:07 +0100

    build: add babel-jest

---
+++ babel.config.ts
@@ -0,0 +1,7 @@
+// noinspection JSUnusedGlobalSymbols
+export default {
+    presets: [
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+        '@babel/preset-typescript',
+    ],
+};
